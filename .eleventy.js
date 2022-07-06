0165a4445f4034a5657a8d9a4681d60b727a8cdf
Advik <asv@duck.com>
2022-07-07 01:17:43 +0530

    done

--- .eleventy.js
+++ .eleventy.js
@@ -76,10 +76,7 @@ module.exports = function(eleventyConfig) {
   });
 
   // Don't process folders with static assets e.g. images
-  eleventyConfig.addPassthroughCopy("favicon.ico");
-  eleventyConfig.addPassthroughCopy("static/img");
   eleventyConfig.addPassthroughCopy("admin");
-  eleventyConfig.addPassthroughCopy("_includes/assets/css/inline.css");
   eleventyConfig.addPassthroughCopy("_includes/assets/");
 
 
